85add638bf7fbf4d2730b9c26d518611295d725a
patt26 <kharchepratik123@gmail.com>
2021-05-04 22:45:07 +0530

    Added Float maximum Method

--- MaxNumberGenerics/MaxNumberGenerics/MaxNumberCheck.cs
+++ MaxNumberGenerics/MaxNumberGenerics/MaxNumberCheck.cs
@@ -22,5 +22,23 @@ namespace MaxNumberGenerics
             }
             throw new Exception("Any 2 Numbers Are same");
         }
+
+        public static double MaximumDoubleNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("Any 2 Numbers Are same");
+        }
+
     }
 }
--- MaxNumberGenerics/MaxNumberGenerics/Program.cs
+++ MaxNumberGenerics/MaxNumberGenerics/Program.cs
@@ -9,6 +9,10 @@ namespace MaxNumberGenerics
             int output = MaxNumberCheck.MaximumIntegerNumber(15, 69, 36);
             Console.WriteLine("The Maximum Integer Number is " + output);
             Console.ReadKey();
+
+            double doubleOutput = MaxNumberCheck.MaximumDoubleNumber(26.7, 49.86, 98.67);
+            Console.WriteLine("The Maximum Float Number is " + doubleOutput);
+            Console.ReadKey();
         }
     }
 }
