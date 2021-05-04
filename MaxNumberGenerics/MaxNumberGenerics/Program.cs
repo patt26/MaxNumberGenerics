6ae88eb00bbd553a701abfcec44b19f9a1dbb9cf
patt26 <kharchepratik123@gmail.com>
2021-05-04 22:41:31 +0530

    Added Integer Maximum Method

---
+++ MaxNumberGenerics/MaxNumberGenerics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaxNumberGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = MaxNumberCheck.MaximumIntegerNumber(15, 69, 36);
+            Console.WriteLine("The Maximum Integer Number is " + output);
+            Console.ReadKey();
+        }
+    }
+}
